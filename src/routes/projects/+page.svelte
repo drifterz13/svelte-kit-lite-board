3de2955cad648f9ac26fb8ca9ccf475d5c1ae2c3
drifterz13 <mixmyxxe@gmail.com>
2022-12-20 20:34:26 +0700

    refactor create project modal

--- src/routes/projects/+page.svelte
+++ src/routes/projects/+page.svelte
@@ -1,4 +1,6 @@
 <script>
+  import CreateProjectModal from './CreateProjectModal.svelte';
+
   /** @type {import('./$types').PageData} */
   export let data;
 </script>
@@ -34,25 +36,4 @@
   {/each}
 </div>
 
-<input type="checkbox" id="create-project-modal" class="modal-toggle" />
-<label for="create-project-modal" class="modal cursor-pointer">
-  <label class="modal-box relative" for="">
-    <form
-      class="form-control w-full mx-auto"
-      method="POST"
-      action="?/createProject"
-    >
-      <label class="label" for="title">
-        <span class="label-text">Project Title</span>
-      </label>
-      <input
-        id="title"
-        class="input input-bordered"
-        name="title"
-        type="text"
-        placeholder="My Project..."
-      />
-      <button class="btn btn-primary mt-5">Create</button>
-    </form>
-  </label>
-</label>
+<CreateProjectModal />
---
+++ src/routes/projects/CreateProjectModal.svelte
@@ -0,0 +1,22 @@
+<input type="checkbox" id="create-project-modal" class="modal-toggle" />
+<label for="create-project-modal" class="modal cursor-pointer">
+  <label class="modal-box relative" for="">
+    <form
+      class="form-control w-full mx-auto"
+      method="POST"
+      action="?/createProject"
+    >
+      <label class="label" for="title">
+        <span class="label-text">Project Title</span>
+      </label>
+      <input
+        id="title"
+        class="input input-bordered"
+        name="title"
+        type="text"
+        placeholder="My Project..."
+      />
+      <button class="btn btn-primary mt-5">Create</button>
+    </form>
+  </label>
+</label>
